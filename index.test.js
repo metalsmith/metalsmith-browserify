5ef2c6c08ebbeb330a93aaa96b752efb2cb81e96
Robert Sköld <robert@publicclass.se>
2016-01-21 11:14:29 +0100

    Accept options to be metalsmith cli-friendly

--- index.test.js
+++ index.test.js
@@ -10,7 +10,7 @@ describe('metalsmith-browserify', function () {
   it('should bundle js files', function (done) {
     metalsmith(__dirname)
       .source('./fixtures/src')
-      .use(browserify('bundle.js'), [
+      .use(browserify('bundle-file.js'), [
         './fixtures/js/index.js'
       ])
       .build(function (err) {
@@ -18,7 +18,56 @@ describe('metalsmith-browserify', function () {
           return done(err);
         }
 
+        expect(fs.existsSync('./build/bundle-file.js')).to.be(true);
+        fs.unlinkSync('./build/bundle-file.js');
+        done();
+      });
+  });
+
+  it('should accept options', function (done) {
+    metalsmith(__dirname)
+      .source('./fixtures/src')
+      .use(browserify({
+        dest: 'bundle-options.js',
+        args: ['./fixtures/js/index.js']
+      }))
+      .build(function (err) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(fs.existsSync('./build/bundle-options.js')).to.be(true);
+        fs.unlinkSync('./build/bundle-options.js');
+        done();
+      });
+  });
+
+  it('should have reasonable default options', function (done) {
+    metalsmith(__dirname)
+      .source('./fixtures/src')
+      .use(browserify())
+      .build(function (err) {
+        if (err) {
+          return done(err);
+        }
+
         expect(fs.existsSync('./build/bundle.js')).to.be(true);
+        fs.unlinkSync('./build/bundle.js');
+        done();
+      });
+  });
+
+  it('should accept true option', function (done) {
+    metalsmith(__dirname)
+      .source('./fixtures/src')
+      .use(browserify(true))
+      .build(function (err) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(fs.existsSync('./build/bundle.js')).to.be(true);
+        fs.unlinkSync('./build/bundle.js');
         done();
       });
   });
